22d6c80aa71c8f8b05061fbc931c1f11d27b06fd
benfenyu <yujingbo_bf@163.com>
2019-06-19 16:56:57 +0800

    20190618 - 移动mess

--- mess/use_ptvsd.py
+++
@@ -1,7 +0,0 @@
-import ptvsd
-
-# 5678 is the default attach port in the VS Code debug configurations
-print("Waiting for debugger attach")
-ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
-ptvsd.wait_for_attach()
-breakpoint()
